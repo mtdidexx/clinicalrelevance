669951139afd4dcfc9db54b947de6c831fb1c66d
Mark Donahue <mark-donahue@idexx.com>
2020-05-15 13:29:33 -0400

    Remove extra annotation

--- src/main/java/net/katrinka/clinicalrelevance/SandboxConfiguration.java
+++ src/main/java/net/katrinka/clinicalrelevance/SandboxConfiguration.java
@@ -1,6 +1,5 @@
 package net.katrinka.clinicalrelevance;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,7 +11,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Profile("sandbox")
 public class SandboxConfiguration {
 
-    @Value("${redisHost}")
     private String redisHost = "10.188.206.107";
 
     @Bean
